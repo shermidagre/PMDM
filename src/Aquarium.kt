b360902846cbe055fefd1ca61158f36dd556e6ca
dam <samuelhermida55@gmail.com>
2025-09-17 11:25:14 +0200

    clase Aquarium.kt con nuevos metodos pero falta arreglar un bug

--- src/Aquarium.kt
+++ src/Aquarium.kt
@@ -1,6 +1,6 @@
 package example.myapp
-
-class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+import java.lang.Math.PI
+class Aquarium(open var length: Int = 100,open var width: Int = 20, open var height: Int = 40) {
 init {
     println("Inicializando aquarium")
 }
@@ -10,7 +10,8 @@ init {
                 "Height: $height cm ")
 
         println("Volume: $volume l")
-
+        // 1 l = 1000 cm^3
+        println("Volume: $volume l Water: $water l (${water/volume*100.0}% full)")
     }
 
     constructor(numberOfFish: Int) : this() {
@@ -19,11 +20,17 @@ init {
 
     }
 
-    var volume: Int
+   open var volume: Int
         get() = width * height * length / 1000  // 1000 cm^3 = 1 l
-        set(value) {
+         set(value) {
             height = (value * 1000) / (width * length)
         }
+
+    open var shape ="recangle"
+    open var water : Double = 0.0
+        get ()= (volume * 1000).toDouble()
+
+
 
 }
 fun buildAquarium() {
@@ -37,10 +44,33 @@ fun buildAquarium() {
     aquarium2.printSize()
     val aquarium3 = Aquarium(29)
     aquarium3.printSize()
+    aquarium3.volume = 70
+    aquarium3.printSize()
+    val aquarium4 = Aquarium(length = 25, width = 25, height = 40)
+    aquarium4.printSize()
+
+    fun buildAquarium() {
+        val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+        myAquarium.printSize()
+        val myTower = TowerTank(diameter = 25, height = 40)
+        myTower.printSize()
+    }
+
+
 }
 
 fun main() {
     buildAquarium()
 }
 
+class TowerTank (override var height: Int, var diameter: Int): Aquarium(height = height, width = diameter, length = diameter) {
+    override var volume: Int
+        // ellipse area = π * r1 * r2
+        get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+        set(value) {
+            height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
+        }
 
+    override var water = volume * 0.8
+    override val shape = "cylinder"
+}
